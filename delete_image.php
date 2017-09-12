b884d84dfa221ff885321a10f7242cc4421fe5d7
NunoLopes96 <nun0lopes@hotmail.com>
2017-09-12 16:53:02 +0100

    add folder verification

--- delete_image.php
+++ delete_image.php
@@ -3,9 +3,20 @@
 require __DIR__ . '/vendor/froala/wysiwyg-editor-php-sdk/lib/FroalaEditor.php';
 
 try {
-  $response = FroalaEditor_Image::delete($_POST['src']);
-  echo stripslashes(json_encode('Success'));
+	$src = $_POST['src'];
+	$upload_folder = "/uploads/";
+	
+	// If the file is inside the uploads folder
+	if (substr($src, 0, strlen($upload_folder)) === $upload_folder)
+	{
+		$response = FroalaEditor_Image::delete($src);
+		echo stripslashes(json_encode('Success'));
+	}
+	else
+	{
+		echo stripslashes(json_encode('Error'));
+	}
 } catch (Exception $e) {
-  echo $e->getMessage();
-  http_response_code(404);
+	echo $e->getMessage();
+	http_response_code(404);
 }
