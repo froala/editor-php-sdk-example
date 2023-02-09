095b36de2019601bf26ef610bee0383407e47dd2
Gangadhar <gangadhar.k@CELSYS217-BLR.CELSYSWTC.IN>
2023-02-09 10:14:49 +0530

    modified deploy.sh with documentation and push image to nexus script,deploy-yes

--- push-image-to-nexus.sh
+++ push-image-to-nexus.sh
@@ -4,11 +4,11 @@ if [ ${TRAVIS_PULL_REQUEST} != "false" ];  then echo "Not publishing a pull requ
 export BRANCH_NAME=`echo "${TRAVIS_BRANCH}" | tr '[:upper:]' '[:lower:]'`
 case "${BRANCH_NAME}" in
         dev*) echo "Branch ${TRAVIS_BRANCH} is eligible for CI/CD" ;;
-	       ao-dev*)echo "Branch ${TRAVIS_BRANCH} is eligible for CI/CD"  ;;
+	ao-dev*)echo "Branch ${TRAVIS_BRANCH} is eligible for CI/CD"  ;;
         qa*) echo "Branch ${TRAVIS_BRANCH} is eligible for CI/CD"  ;;
-	       qe*) echo "Branch ${TRAVIS_BRANCH} is eligible for CI/CD"  ;;
-	       rc*) echo "Branch ${TRAVIS_BRANCH} is eligible for CI/CD"  ;;
-	       release-master*) echo "Branch ${TRAVIS_BRANCH} is eligible for CI/CD"  ;;
+	qe*) echo "Branch ${TRAVIS_BRANCH} is eligible for CI/CD"  ;;
+	rc*) echo "Branch ${TRAVIS_BRANCH} is eligible for CI/CD"  ;;
+	release-master*) echo "Branch ${TRAVIS_BRANCH} is eligible for CI/CD"  ;;
         ft*) echo "Branch ${TRAVIS_BRANCH} is eligible for CI" ;;
         bf*) echo "Branch ${TRAVIS_BRANCH} is eligible for CI" ;;
         *) echo "Not a valid branch name for CI/CD" && exit -1;;
@@ -28,7 +28,10 @@ PHP_SDK_NAME=`jq '.php_sdk_name' version.json | tr -d '"'`
 PHP_BRANCH_NAME=`jq '.php_sdk_branch' version.json | tr -d '"'` 
 PHP_SDK_GIT_URL=`jq '.php_sdk_repo' version.json | tr -d '"'`
 echo "SDK: ${PHP_SDK_NAME}  , branch: ${PHP_BRANCH_NAME}  ,  version: ${PACKAGE_VERSION} , git url: ${PHP_SDK_GIT_URL}"
-docker build -t ${IMAGE_NAME}:${SHORT_COMMIT} --build-arg PackageName=${PACKAGE_NAME} --build-arg PackageVersion=${PACKAGE_VERSION} --build-arg NexusUser=${NEXUS_USER} --build-arg NexusPassword=${NEXUS_USER_PWD} --build-arg GitUsr=${GITHUB_USR} --build-arg GitToken=${GITHUB_TOKEN}  --build-arg PHPsdkGitURL=${PHP_SDK_GIT_URL} --build-arg PHPsdkBranch=${PHP_BRANCH_NAME}   .
+echo "IP address of BUILD agent is: " && hostname -I
+echo "Hostname of BUILD agent is: " && hostname
+docker image prune -f
+DOCKER_BUILDKIT=0 docker build --no-cache -t ${IMAGE_NAME}:${SHORT_COMMIT} --build-arg PackageName=${PACKAGE_NAME} --build-arg PackageVersion=${PACKAGE_VERSION} --build-arg NexusUser=${NEXUS_USER} --build-arg NexusPassword=${NEXUS_USER_PWD} .
 sleep 3
 docker image ls 
 echo "uploading to nexus  ${NEXUS_CR_TOOLS_URL}/froala-${IMAGE_NAME}:${PACKAGE_VERSION} "
